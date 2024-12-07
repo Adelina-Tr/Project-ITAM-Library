92112a62f5b499e7397ccf3796c4a97379910e2c
Adelina Trejo <kf8@hotmail.com>
2024-12-07 11:17:40 -0600

    first commit

---
+++ Proyecto Biblioteca/src/EjecutableBibliotecaConVentana.java
@@ -0,0 +1,19 @@
+
+public class EjecutableBibliotecaConVentana {
+	/*
+	 * Adelina Trejo Espinoza 168135
+	 * Proyecto Final BIBLIOTECA CENTRAL
+	 * Ejecutable de la ventana con funcionalidad
+	 * 07/12/16
+	 */
+
+	public static void main(String[] args) {
+		
+		BibliotecaControlador miVentana;
+		
+		miVentana= new BibliotecaControlador();
+		miVentana.pack();
+		
+	}
+
+}
